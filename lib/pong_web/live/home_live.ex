e8eff913d824adf76ebac756adcbd923efd5a242
Daniel Ramírez-Chiquillo <hi@danielrachi.com>
2024-10-22 21:44:17 -0500

    Generate room number
    Added number generating functionality to the home page.
    
    The room numbers will be used for joing and identifying a game.
    
    Resolves: #21
    See also: #1

--- lib/pong_web/live/home_live.ex
+++ lib/pong_web/live/home_live.ex
@@ -2,6 +2,12 @@ defmodule PongWeb.HomeLive do
   use PongWeb, :live_view
 
   def mount(_params, _session, socket) do
+    room_code = generate_code()
+    socket = assign(socket, :room_code, room_code)
     {:ok, socket, layout: false}
+  end
+
+  defp generate_code() do
+    :rand.uniform(900_000) + 100_000
   end
 end
